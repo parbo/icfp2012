dbf6e821f2c39683ddb7c8861b48409062afe9ac
Pär Bohrarper <parbo@icfp2012.(none)>
2012-07-13 17:35:25 +0200

    rename some stuff

--- src/viewer.py
+++ src/viewer.py
@@ -22,40 +22,40 @@ class Viewer(wx.Frame):
         self.SetBackgroundColour(wx.LIGHT_GREY)
         self.SetMinSize((500, 300))
         # Child control initializations.
-        self.mapInput = wx.TextCtrl(self, -1, map_file)
-        self.mapLabel = wx.StaticText(self, -1, 'Map file:')
-        self.mapSelectBtn = wx.Button(self, ID_MAP_SELECT_BTN, 'Select')
-        self.routeInput = wx.TextCtrl(self, -1, route)
-        self.routeLabel = wx.StaticText(self, -1, 'Route:')
-        self.loadBtn = wx.Button(self, ID_LOAD_BTN, 'Load')
-        self.stepBtn = wx.Button(self, ID_STEP_BTN, 'Step')
-        self.stepInput = wx.SpinCtrl(self, -1, '1', min=1, max=3000000, initial=1)
-        self.runBtn = wx.Button(self, ID_RUN_BTN, 'Run')
-        self.canvas = Canvas(self)
-        self.canvas.EnableScrolling(True, True)
+        self._map_input = wx.TextCtrl(self, -1, map_file)
+        self._map_label = wx.StaticText(self, -1, 'Map file:')
+        self._map_select_btn = wx.Button(self, ID_MAP_SELECT_BTN, 'Select')
+        self._route_input = wx.TextCtrl(self, -1, route)
+        self._route_label = wx.StaticText(self, -1, 'Route:')
+        self._load_btn = wx.Button(self, ID_LOAD_BTN, 'Load')
+        self._step_btn = wx.Button(self, ID_STEP_BTN, 'Step')
+        self._step_input = wx.SpinCtrl(self, -1, '1', min=1, max=3000000, initial=1)
+        self._run_btn = wx.Button(self, ID_RUN_BTN, 'Run')
+        self._canvas = Canvas(self)
+        self._canvas.EnableScrolling(True, True)
         # Sizer layout.
-        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
-        self.mapSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.routeSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.canvasAreaSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.canvasSizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.commandSizer = wx.BoxSizer(wx.VERTICAL)
-        self.mainSizer.Add(self.mapSizer, 0, flag = wx.EXPAND)
-        self.mainSizer.Add(self.routeSizer, 0, flag = wx.EXPAND)
-        self.mainSizer.Add(self.canvasAreaSizer, 1, flag = wx.EXPAND)
-        self.mapSizer.Add(self.mapLabel, 1, flag = wx.ALIGN_LEFT)
-        self.mapSizer.Add(self.mapInput, 7, flag = wx.EXPAND)
-        self.mapSizer.Add(self.mapSelectBtn, 0, flag = wx.ALIGN_RIGHT)
-        self.routeSizer.Add(self.routeLabel, 1, flag = wx.ALIGN_LEFT)
-        self.routeSizer.Add(self.routeInput, 7, flag = wx.EXPAND)
-        self.canvasAreaSizer.Add(self.commandSizer, 0, flag = wx.EXPAND)
-        self.canvasAreaSizer.Add(self.canvasSizer, 1, flag = wx.EXPAND)
-        self.canvasSizer.Add(self.canvas, 1, flag = wx.EXPAND)
-        self.commandSizer.Add(self.loadBtn, 0, flag = wx.EXPAND)
-        self.commandSizer.Add(self.stepBtn, 0, flag = wx.EXPAND)
-        self.commandSizer.Add(self.stepInput, 0, flag = wx.EXPAND)
-        self.commandSizer.Add(self.runBtn, 0, flag = wx.EXPAND)
-        self.SetSizer(self.mainSizer)
+        self._main_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._map_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._route_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._canvas_area_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._canvas_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self._command_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._main_sizer.Add(self._map_sizer, 0, flag = wx.EXPAND)
+        self._main_sizer.Add(self._route_sizer, 0, flag = wx.EXPAND)
+        self._main_sizer.Add(self._canvas_area_sizer, 1, flag = wx.EXPAND)
+        self._map_sizer.Add(self._map_label, 1, flag = wx.ALIGN_LEFT)
+        self._map_sizer.Add(self._map_input, 7, flag = wx.EXPAND)
+        self._map_sizer.Add(self._map_select_btn, 0, flag = wx.ALIGN_RIGHT)
+        self._route_sizer.Add(self._route_label, 1, flag = wx.ALIGN_LEFT)
+        self._route_sizer.Add(self._route_input, 7, flag = wx.EXPAND)
+        self._canvas_area_sizer.Add(self._command_sizer, 0, flag = wx.EXPAND)
+        self._canvas_area_sizer.Add(self._canvas_sizer, 1, flag = wx.EXPAND)
+        self._canvas_sizer.Add(self._canvas, 1, flag = wx.EXPAND)
+        self._command_sizer.Add(self._load_btn, 0, flag = wx.EXPAND)
+        self._command_sizer.Add(self._step_btn, 0, flag = wx.EXPAND)
+        self._command_sizer.Add(self._step_input, 0, flag = wx.EXPAND)
+        self._command_sizer.Add(self._run_btn, 0, flag = wx.EXPAND)
+        self.SetSizer(self._main_sizer)
         # Status bar definitions.
         bar = self.CreateStatusBar(8)
         bar.SetStatusWidths([-2, -2, -2, -2, -2, -2, -2, -3])
@@ -76,48 +76,52 @@ class Viewer(wx.Frame):
     # Event handlers.
     def OnMapSelectBtn(self, event):
         #print 'OnMapSelectBtn'
-        dlg = wx.FileDialog(self, 'Open map file', wildcard = 'Map (*.map)|*.map', style = wx.OPEN | wx.FILE_MUST_EXIST | wx.CHANGE_DIR)
+        dlg = wx.FileDialog(self,
+                            'Open map file',
+                            wildcard = 'Map (*.map)|*.map',
+                            style = wx.OPEN | wx.FILE_MUST_EXIST | wx.CHANGE_DIR)
         if dlg.ShowModal() == wx.ID_OK:
-            self.mapInput.SetValue(dlg.GetPath())
+            self._map_input.SetValue(dlg.GetPath())
         dlg.Destroy()
 
     def OnLoadBtn(self, event):
         #print 'OnLoadBtn'
-        map_path = self.mapInput.GetValue()
+        map_path = self._map_input.GetValue()
         f = open(map_path)
         map_def = f.readlines()
+        f.close()
         w = max((len(line) - 1 for line in map_def))
         h = len(map_def)
         self.sim = []
         for line in map_def:
             self.sim.append(line[0:-1] + " "*(w-(len(line)-1)))
-        self.canvas.SetMapSize(w, h)
+        self._canvas.SetMapSize(w, h)
         self.UpdateStatusBar()
 
     def OnStepBtn(self, event):
         #print 'OnStepBtn'
-        self.Run(int(self.stepInput.GetValue()))
+        self.Run(int(self._step_input.GetValue()))
 
     def OnRunBtn(self, event):
         #print 'OnRunBtn'
         if self.sim_running:
             self.sim_running = False
-            self.runBtn.SetLabel('Run')
+            self._run_btn.SetLabel('Run')
         else:
             self.sim_running = True
-            self.runBtn.SetLabel('Stop')
+            self._run_btn.SetLabel('Stop')
             self.AddPendingEvent(RunSimEvent(id=ID_RUN_EVENT))
 
     def OnRunEvent(self, event):
         #print 'OnRunEvent'
-        self.Run(int(self.stepInput.GetValue()))
+        self.Run(int(self._step_input.GetValue()))
 
     def OnClose(self, event):
         #print 'OnClose'
         self.Destroy()
 
     def Run(self, steps):
-        self.canvas.Refresh()
+        self._canvas.Refresh()
         self.UpdateStatusBar()
 
     def UpdateStatusBar(self):
@@ -129,14 +133,14 @@ class Canvas(wx.ScrolledWindow):
         self.Bind(wx.EVT_SIZE, self.OnSize)
         self.Bind(wx.EVT_PAINT, self.OnPaint)
         # load bitmaps
-        self.bmpClosedLift = wx.Bitmap(os.path.join("images", "closed-lift.jpg"));
-        self.bmpEarth = wx.Bitmap(os.path.join("images", "earth.jpg"));
-        self.bmpEmpty = wx.Bitmap(os.path.join("images", "empty.jpg"));
-        self.bmpLambda = wx.Bitmap(os.path.join("images", "lambda.jpg"));
-        self.bmpOpenLift = wx.Bitmap(os.path.join("images", "open-lift.jpg"));
-        self.bmpRobot = wx.Bitmap(os.path.join("images", "robot.jpg"));
-        self.bmpRock = wx.Bitmap(os.path.join("images", "rock.jpg"));
-        self.bmpWall = wx.Bitmap(os.path.join("images", "wall.jpg"));
+        self._bmp_closed_lift = wx.Bitmap(os.path.join("images", "closed-lift.jpg"));
+        self._bmp_earth = wx.Bitmap(os.path.join("images", "earth.jpg"));
+        self._bmp_empty = wx.Bitmap(os.path.join("images", "empty.jpg"));
+        self._bmp_lambda = wx.Bitmap(os.path.join("images", "lambda.jpg"));
+        self._bmp_open_lift = wx.Bitmap(os.path.join("images", "open-lift.jpg"));
+        self._bmp_robot = wx.Bitmap(os.path.join("images", "robot.jpg"));
+        self._bmp_rock = wx.Bitmap(os.path.join("images", "rock.jpg"));
+        self._bmp_wall = wx.Bitmap(os.path.join("images", "wall.jpg"));
         # Ensure scrollbars are used
         self.SetMapSize(10, 10)
 
@@ -153,30 +157,30 @@ class Canvas(wx.ScrolledWindow):
         dc.Clear()
         parent = self.GetParent()
         if parent.sim is not None:
-            def bmpFromObj(obj):
-                return {"#": self.bmpWall,
-                        "R": self.bmpRobot,
-                        "*": self.bmpRock,
-                        "\\": self.bmpLambda,
-                        "L": self.bmpClosedLift,
-                        "O": self.bmpOpenLift,
-                        ".": self.bmpEarth,
-                        " ": self.bmpEmpty}[obj]
+            def bmp_from_obj(obj):
+                return {"#": self._bmp_wall,
+                        "R": self._bmp_robot,
+                        "*": self._bmp_rock,
+                        "\\": self._bmp_lambda,
+                        "L": self._bmp_closed_lift,
+                        "O": self._bmp_open_lift,
+                        ".": self._bmp_earth,
+                        " ": self._bmp_empty}[obj]
             for y, line in enumerate(parent.sim):
                 for x, pos in enumerate(line):
-                    bmp = bmpFromObj(pos)
+                    bmp = bmp_from_obj(pos)
                     p = self.CalcScrolledPosition((x*16, y*16))
                     dc.DrawBitmap(bmp, p.x, p.y, True)
 
     def SetMapSize(self, xw, yw):
-        self.xp = 16 * xw
-        self.yp = 16 * yw
+        self._xp = 16 * xw
+        self._yp = 16 * yw
         self.UpdateMapSize()
 
     def UpdateMapSize(self):
         # Size in world coordinates.
-        self.SetClientSize((self.xp, self.yp))
-        self.SetScrollbars(1, 1, self.xp, self.yp)
+        self.SetClientSize((self._xp, self._yp))
+        self.SetScrollbars(1, 1, self._xp, self._yp)
         self.SetScrollRate(16, 16)
         self.Refresh()
         #print 'Set world size:', self.xw / EARTH_RADIUS, self.yw / EARTH_RADIUS, 'Scale = %.3e m/pxl' % scale
